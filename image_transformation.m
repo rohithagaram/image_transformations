5a597acf486a80425598f22bd9ea97838007860f
Rohith <rohithagaram@gmail.com>
2021-07-02 00:16:37 +0530

    Added non uniform scaling

--- image_transformation.m
+++ image_transformation.m
@@ -1,4 +1,4 @@
-function image_transformation (image_name,image_path,operation,scale_factor)
+function image_transformation (image_name,image_path,operation,scale_factor,x_scale,y_scale)
 
   path = image_path + "\" +image_name ;
   input_img = imread(path);
@@ -22,7 +22,21 @@ function image_transformation (image_name,image_path,operation,scale_factor)
               end
           end
       end
-              
+  elseif(operation == "non_uniform_scale")
+      scaled_width = round(width/x_scale);
+      scaled_height = round(height/y_scale);
+      final_img = uint8(zeros(scaled_height,scaled_width,3));
+      for i = 1:scaled_height
+          for j= 1:scaled_width
+              if((round(i*y_scale)<height) && (round(j*x_scale) <width))
+                final_image(i,j,:) = input_img(round(i*y_scale),round(j*x_scale),:);
+              elseif ((round(i*y_scale)<height))
+                final_image(i,j,:) = final_image(i,j-1,:);
+              else 
+                final_image(i,j,:) =  final_image(i-1,j,:);
+              end
+          end
+      end
   end
   figure ;
   imshow(final_image);
